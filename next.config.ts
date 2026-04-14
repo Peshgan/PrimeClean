ff3c7a792d8642dfb0329d33a3bc75b1739c574b
MrPeshgan <pablonewsgm@gmail.com>
2026-04-14 21:25:53 +0300

    Работа с изображениями

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "cdn.primeclean.by" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
   async headers() {
